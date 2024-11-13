0c2f552a00a8b5397860f5838d37d5fb1b5dbcff
techjmi <contactshamim62@gmail.com>
2024-11-13 17:07:12 +0530

    cors

--- index.js
+++ index.js
@@ -9,11 +9,17 @@ const event_rouet= require('./routes/event_routes')
 
 connectDB() 
 app.use(cors())
+// app.use(cors({
+//     origin: 'https://calendar-8nsa.onrender.com',  // Allow requests from this specific origin
+//     methods: 'GET,POST,PUT,DELETE', // Specify allowed HTTP methods
+//     credentials: true // Enable credentials if needed
+//   }));
 app.use(cors({
-    origin: 'https://calendar-8nsa.onrender.com',  // Allow requests from this specific origin
-    methods: 'GET,POST,PUT,DELETE', // Specify allowed HTTP methods
-    credentials: true // Enable credentials if needed
-  }));
+  origin: [/\.render\.com$/], // Allow requests from all subdomains on Render
+  methods: 'GET,POST,PUT,DELETE',
+  credentials: true,
+}));
+
 app.use(express.json())
 app.use('/calendar', event_rouet)  
 app.listen(PORT,()=>{
